829078d940eac6c1393652e2aa7bd6376539e263
Antoine Brunet <contact@toinebru.net>
2017-11-08 15:02:49 +0100

    Safe interupt

--- include/gestion_parabole/gestion_parabole.h
+++ include/gestion_parabole/gestion_parabole.h
@@ -12,7 +12,7 @@ using json=nlohmann::json;
 
 namespace fyt_par {
 	typedef geometry_msgs::Quaternion quaternion_t;
-	typedef ros::Duration timing_t;
+	typedef int timing_t;
 	typedef const json & params_t;
 
 	class Manoeuvre {
@@ -58,7 +58,7 @@ namespace fyt_par {
 				guidage.object_tracking = true;
 				ctx.publish(guidage);
 
-				t2.sleep();
+				ctx.sleep(t2);
 			}
 	};
 
@@ -66,13 +66,14 @@ namespace fyt_par {
 		private:
 			timing_t t1,t2;
 			quaternion_t qdec;
-			std::vector<double> kp, kv;
+			std::vector<double> kp, kv1, kv2;
 			Context ctx;
 		public:
 			FixedPointing(Context ctx_, params_t p, timing_t ts, quaternion_t qd) : 
 				ctx(ctx_),
 				kp((const std::vector<double> &)(p["kp"])), 
-				kv((const std::vector<double> &)(p["kv"])), 
+				kv1((const std::vector<double> &)(p["kv1"])), 
+				kv2((const std::vector<double> &)(p["kv2"])), 
 				t1(ts), t2((float)(p["tfin"])),
 				qdec(qd) {}
 
@@ -81,7 +82,7 @@ namespace fyt_par {
 				guidage.a = {0,0,0};
 				guidage.b = {0,0,0};
 				guidage.kp = {0,0,0};
-				std::copy(kv.begin(), kv.end(), guidage.kv.begin());
+				std::copy(kv1.begin(), kv1.end(), guidage.kv.begin());
 				guidage.qd.x = 0;
 				guidage.qd.y = 0;
 				guidage.qd.z = 0;
@@ -91,19 +92,19 @@ namespace fyt_par {
 				std::cout << "Publishing 1st guidance order" << std::endl;
 
 				if (t1 < t2) {
-					t1.sleep();
+					if (!ctx.sleep(t1)) { return; }
 
 					quaternion_t qm = ctx.get_qm();
 					guidage.qd = qmult(qm, qdec);
 					std::copy(kp.begin(), kp.end(), guidage.kp.begin());
-					guidage.kv = {0,0,0};
+					std::copy(kv2.begin(), kv2.end(), guidage.kv.begin());
 					ctx.publish(guidage);
 					std::cout << "Publishing 2nd guidance order" << std::endl;
 
 					timing_t dt = t2-t1;
-					dt.sleep();
+					ctx.sleep(dt);
 				} else {
-					t2.sleep();
+					ctx.sleep(t2);
 				}
 			}
 	};
--- src/gestion_parabole_node.cpp
+++ src/gestion_parabole_node.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <mutex>
 
 #include "gestion_parabole/gestion_parabole.h"
 #include "cmg_msgs/State.h"
@@ -17,8 +18,10 @@ class Controller {
 		ros::Subscriber sub_qm, sub_state;
 		ManoeuvreFactory<const Controller&> mf;
 		quaternion_t qm;
+		mutable std::mutex qm_mut;
 		params_t agcs;
 		params_t paraboles;
+		bool alert, running;
 		unsigned int current_parabole;
 	public:
 		Controller(params_t conf) : 
@@ -35,9 +38,11 @@ class Controller {
 			current_parabole = 0;
 		}
 		quaternion_t get_qm() const {
+			std::unique_lock<std::mutex> lock(qm_mut);
 			return qm;
 		}
 		void set_qm(const quaternion_t::ConstPtr& msg) {
+			std::unique_lock<std::mutex> lock(qm_mut);
 			qm = *msg;
 		}
 		void get_ready(params_t agc) {
@@ -62,17 +67,44 @@ class Controller {
 			int curr_agc = pbc["ag_config"];
 			params_t agc = agcs[curr_agc];
 			if (msg->state == STATE_READY) {
+				ROS_INFO("Loading ag config number %d",curr_agc);
 				get_ready(agc);	
 			}
 			if (msg->state == STATE_MISS) {
-				mf.run(agc, pbc);
+				if (!running) {
+					alert = false;
+					running = true;
+					ROS_INFO("Starting maneuver");
+					mf.run(agc, pbc);
+					if (alert) {
+						ROS_WARN("Mission was interupted (went to safe state)");
+					}
+					running = false;
+				} else {
+					ROS_ERROR("New mission started but previous one was not done.");
+				}
 			}
 			if (msg->state == STATE_POST) {
 				current_parabole++;
 			}
+			if (msg->state == STATE_SAFE) {
+				ROS_INFO("Going to safe state, alerting current maneuver");
+				alert = true;
+			}
 		}
 		void publish(const cmg_msgs::Guidage& guidage) const {
 			pub_guidage.publish(guidage);
+		}
+		bool sleep(timing_t t) const {
+			int poll_freq = 3;
+			int max = t * poll_freq + 1;
+			ros::Rate r(poll_freq);
+			for (int i = 1; (i < max) && (!alert); i++) {
+				ros::spinOnce();
+				r.sleep();
+				ROS_INFO("tic tac %d/%d",i,max);
+			}
+			return !alert;
 		}
 };
 
@@ -83,14 +115,5 @@ int main(int argc, char * argv[]) {
 	in >> j;
 
 	Controller c(j);
-/*	ManoeuvreFactory<const Controller&> mf(j["globals"], c);
-	
-	json paraboles = j["paraboles"];
-	for (int i = 0; i < paraboles.size(); i++) {
-		try {
-			mf.run(paraboles[i]);
-		} catch (std::exception & e) {
-			std::cout << e.what() << std::endl;
-		}
-	}*/
+	ros::spin();
 }
